feb13a1c88bfad44f881fe593f989ea9dca1c12c
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2022-09-16 12:36:02 +0900

    “”

---
+++ server.js
@@ -0,0 +1,27 @@
+//console.log(("Hello"))
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+    // res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("ユーザーが接続しました");
+
+    socket.on("chat message", (msg) => {
+        // console.log("メッセージ: " + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+});
